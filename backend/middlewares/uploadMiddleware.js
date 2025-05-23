761202f8248780b07be3a1390ea92f56604258f9
RISHIGOVIND <rishigovind36@gmail.com>
2025-05-23 11:05:21 +0530

    Updated

--- backend/middlewares/uploadMiddleware.js
+++ backend/middlewares/uploadMiddleware.js
@@ -1,28 +1,31 @@
 const multer = require("multer");
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/");
-  },
-  filename: (req, file, cb) => {
-    cb(null, `${Date.now()}-${file.originalname}`);
-  },
-});
+const storage = multer.memoryStorage();
 
-const filefilter = (req, file, cb) => {
-  const allowedTypes = ["image/jpeg", "image/png", "image/jpg"];
-  if (allowedTypes.includes(file.mimetype)) {
-    cb(null, true);
-  } else {
-    cb(new Error("Only .jpeg,.jpg and .png format are allowed"), false);
-  }
-};
+
+// {
+//   destination: (req, file, cb) => {
+//     cb(null, "uploads/");
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, `${Date.now()}-${file.originalname}`);
+//   },
+// }
+
+// const filefilter = (req, file, cb) => {
+//   const allowedTypes = ["image/jpeg", "image/png", "image/jpg"];
+//   if (allowedTypes.includes(file.mimetype)) {
+//     cb(null, true);
+//   } else {
+//     cb(new Error("Only .jpeg,.jpg and .png format are allowed"), false);
+//   }
+// };
 
 const upload =
   multer (
   {
     storage,
-    filefilter,
+    // filefilter,
   });
 
 module.exports = upload;
