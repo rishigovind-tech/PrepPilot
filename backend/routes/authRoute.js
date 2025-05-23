f21c27c4563a0300949e38aaf715ad7976177a0b
RISHIGOVIND <rishigovind36@gmail.com>
2025-05-23 10:25:03 +0530

    Updated

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,24 +1,31 @@
-const express=require("express")
-const {registerUser,loginUser,getuserProfile}=require("../controllers/authController")
-const {protect}=require("../middlewares/authMiddleware");
+const express = require("express");
+const {
+  registerUser,
+  loginUser,
+  getuserProfile,
+} = require("../controllers/authController");
+const { protect } = require("../middlewares/authMiddleware");
 const upload = require("../middlewares/uploadMiddleware");
+const cloudinary = require("../config/cloudinaryConfig");
 
-const router=express.Router()
+const router = express.Router();
 
-router.post("/register",registerUser);
-router.post("/login",loginUser)
-router.get("/profile",protect,getuserProfile)
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.get("/profile", protect, getuserProfile);
 
-router.post("/upload-image",upload.single("image"),(req,res)=>{
-    if(!req.file){
-        return res.status(400).json({message:"No file Uploaded"})
-    }
+router.post("/upload-image", upload.single("image"), async (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ message: "No file Uploaded" });
+  }
 
-    const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${req.file.filename}`;
+  const imageUrl = await cloudinary.uploader.upload(req.file.path);
 
+//   const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${
+//     req.file.filename
+//   }`;
 
-    res.status(200).json({imageUrl})
-})
+  res.status(200).json({ imageUrl });
+});
 
-module.exports=router;
-
+module.exports = router;
